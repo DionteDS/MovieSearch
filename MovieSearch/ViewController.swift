9673652fd4467e71e37090239edf6cbbd6708c27
Dionte <Dionte_sb@yahoo.com>
2020-01-09 11:22:57 -0800

    query data for now playing movies.

--- MovieSearch/ViewController.swift
+++ MovieSearch/ViewController.swift
@@ -13,14 +13,40 @@ import SwiftyJSON
 
 class ViewController: UIViewController {
     
+    let apikey = "INSERT YOUR API KEY HERE"
+    let baseURLForNowPlaying = "https://api.themoviedb.org/3/movie/now_playing"
+    let baseURLImageString = "https://image.tmdb.org/t/p/w500"
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setQuery()
     }
 
+    func setQuery() {
+        
+        let params: [String: String] = ["api_key": APIKEY, "language": "en-US", "page": "1"]
+        
+        fetchData(url: baseURLForNowPlaying, paramemters: params)
+        
+    }
     
+    func fetchData(url: String, paramemters: [String: String]) {
+        
+        Alamofire.request(url, method: .get, parameters: paramemters).responseJSON {
+            response in
+            
+            if let responseValue = response.result.value as! [String: Any]? {
+                if let responseMovies = responseValue["results"] as! [[String: Any]]? {
+                    print(responseMovies)
+                    
+                }
+                
+            }
+        }
+        
+    }
 
 }
 
