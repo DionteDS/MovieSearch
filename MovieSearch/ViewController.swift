56e6a8a2d3f5fa19c6e878b7e1678de6385c03b5
Dionte <Dionte_sb@yahoo.com>
2020-01-09 11:33:36 -0800

    Implemented tableview to hold movie titles

--- MovieSearch/ViewController.swift
+++ MovieSearch/ViewController.swift
@@ -13,13 +13,22 @@ import SwiftyJSON
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var movieTableView: UITableView!
+    
+    
     let apikey = "INSERT YOUR API KEY HERE"
     let baseURLForNowPlaying = "https://api.themoviedb.org/3/movie/now_playing"
     let baseURLImageString = "https://image.tmdb.org/t/p/w500"
     
+    var movies: [[String: Any]] = [[String: Any]]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.title = "Now Playing"
+        
+        movieTableView.rowHeight = 100.0
         
         setQuery()
     }
@@ -39,8 +48,10 @@ class ViewController: UIViewController {
             
             if let responseValue = response.result.value as! [String: Any]? {
                 if let responseMovies = responseValue["results"] as! [[String: Any]]? {
-                    print(responseMovies)
-                    
+                    self.movies = responseMovies
+                    DispatchQueue.main.async {
+                        self.movieTableView.reloadData()
+                    }
                 }
                 
             }
@@ -50,3 +61,23 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell", for: indexPath)
+        
+        let eachMovie = self.movies[indexPath.row]
+        
+        cell.textLabel?.text = (eachMovie["title"] as? String) ?? ""
+        
+        return cell
+        
+    }
+    
+    
+}
