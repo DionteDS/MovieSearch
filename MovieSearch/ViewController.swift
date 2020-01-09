c5a92b69633e339ccb13b67add981774e8bd8ec1
Dionte <Dionte_sb@yahoo.com>
2020-01-09 10:34:31 -0800

    Initial commit

---
+++ MovieSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieSearch
+//
+//  Created by Dionte Silmon on 1/9/20.
+//  Copyright © 2020 Dionte Silmon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
